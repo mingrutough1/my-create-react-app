11e96419a7198630a8a28318467c94da373a2c00
mingrutough <mingru.fe@foxmail.com>
2019-07-12 11:25:35 +0800

    host

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,6 +4,8 @@ module.exports = function(app) {
   app.use(
     proxy("/kyfadm-api/**", {
       target: "http://10.25.73.2/", // 开发环境
+    //   target: "http://csms-polaris-app-stg4.paic.com.cn/", // 测试环境
+    //   target: "http://csms-polaris-app.paic.com.cn/", // 生产环境
       changeOrigin: true
     })
   );
