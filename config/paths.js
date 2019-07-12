88ff0b1f5086634d07ac20c15e780ae687dae0a0
mingrutough <mingru.fe@foxmail.com>
2019-07-12 14:01:40 +0800

    打包

--- config/paths.js
+++ config/paths.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const path = require('path');
 const fs = require('fs');
@@ -8,7 +8,7 @@ const url = require('url');
 // https://github.com/facebook/create-react-app/issues/637
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
-
+process.env.PUBLIC_URL = '/kyfadm';
 const envPublicUrl = process.env.PUBLIC_URL;
 
 function ensureSlash(inputPath, needsSlash) {
