238c0baa1967346d7cd1b23a97417de7f0fefa1b
mingrutough <mingru.fe@foxmail.com>
2019-07-12 15:30:55 +0800

    打包

---
+++ web/precache-manifest.0237fca999fb0c0e9162c72e5986dde7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21cd34735bdb2af6f6e9f93852ddacc6",
+    "url": "/kyfadm/index.html"
+  },
+  {
+    "revision": "2e984f0fb955f17d09c6",
+    "url": "/kyfadm/static/css/2.dcc683d0.chunk.css"
+  },
+  {
+    "revision": "a5289c83678a0f686062",
+    "url": "/kyfadm/static/css/main.b46439f3.chunk.css"
+  },
+  {
+    "revision": "2e984f0fb955f17d09c6",
+    "url": "/kyfadm/static/js/2.72bbf6c7.chunk.js"
+  },
+  {
+    "revision": "a5289c83678a0f686062",
+    "url": "/kyfadm/static/js/main.35cd8ffb.chunk.js"
+  },
+  {
+    "revision": "3a61e8b91c35abd65e2a",
+    "url": "/kyfadm/static/js/runtime~main.349d5ec2.js"
+  }
+]);
